46dc5f62eb819ff85a7857ecdd19ab952947fc05
Phil Poore <phil.j.poore@gmail.com>
2020-12-29 23:03:41 +0000

    feat: _template

---
+++ _template/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "Example.h"
+
+int main() {
+    std::cout << "Hello world\n";
+}
---
+++ _template/src/tests.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <assert.h>
+#include <gtest/gtest.h>
+#include <string>
+
+#include "Example.h"
+
+TEST(Example, one) {
+    EXPECT_EQ(true, true);
+}
